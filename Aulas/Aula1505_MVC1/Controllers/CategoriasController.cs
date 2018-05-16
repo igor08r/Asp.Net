d6d710f2aac7863d38e19d67e28b809ac190fd36
1712925 <1712925@DCIC116962>
2018-05-15 22:03:14 -0300

    novo projeto  - view/controller/model

---
+++ Aulas/Aula1505_MVC1/Controllers/CategoriasController.cs
@@ -0,0 +1,27 @@
+﻿using Aula1505_MVC1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula1505_MVC1.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            List<Categoria> categorias = new List<Categoria>();
+
+            categorias.Add(new Categoria() { Nome = "Casa"});
+            categorias.Add(new Categoria() { Nome = "Carro"});
+            categorias.Add(new Categoria() { Nome = "Trabalho"});
+            categorias.Add(new Categoria() { Nome = "Família"});
+
+            ViewBag.MinhasCategorias = categorias;
+
+            return View();
+        }
+    }
+}
